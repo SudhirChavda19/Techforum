fe57e9303e224236eac8d779cdc85f4cfc7bf01d
Sudhir <sudhirahir9329@gmail.com>
2023-08-18 02:12:16 +0530

    chai mocha updated

--- techforumbackend/test/test.spec.js
+++ techforumbackend/test/test.spec.js
@@ -7,10 +7,15 @@ chai.use(chaihttp);
 
 const app = require("../app");
 
-describe("Testing for question", () => {
-    it("GET /users/question/", (done) => {
-        chai.request(app).get("/users/question/").end((err, res) => {
-            res.should.have.status(404);
+describe("Testing for question", (suite) => {
+    suite.timeout(0);
+    it("GET all Question", (done) => {
+        chai.request(app).get("/api/users/question").end((err, res) => {
+            res.should.have.status(200);
+            // res.body.should.be.a('array');
+            // res.body.length.should.be.eql(0);
+            // res.should.have.keys("message", "data", "status");
+            done();
         });
     });
 });
